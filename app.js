3d6c77d0df39f8b096c5a2f00de694685303735b
aruntiwaribbk <tiwariarunbbk@gmail.com>
2016-08-23 19:12:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -53,24 +53,23 @@ $(document).ready(function() {
         hsp.showFollowDialog('Hootsuite', true);
     });
 
-    $('._customUserInfo').click(function(){
-        hsp.customUserInfo({
-          "fullName": "arun",
-          "screenName": "@arun22755309",
+     hsp.customUserInfo({
+          "fullName": "David Chan",
+          "screenName": "@chandavid",
           "avatar": "https://d1cmhiswqj5a7e.cloudfront.net/http%3A%2F%2Fplacehold.it%2F30x30%2F444",
-          "profileUrl": "https://twitter.com/arun22755309",
-          "userLocation": "India",
-          "bio": ".Net Developer",
+          "profileUrl": "http://twitter.com/chandavid",
+          "userLocation": "Vancouver, BC",
+          "bio": "JavaScript/web/martini developer. Working on @Hootsuite. Making by breaking.",
           "extra": [
             {"label": "Age", "value": "Unknown"},
             {"label": "Gender", "value": "Male"}
           ],
           "links": [
-            {"label": "Hootsuite", "url": "https://hootsuite.com"},
-            {"label": "Blog", "url": "https://blog.hootsuite.com"}
+            {"label": "Hootsuite", "url": "http://hootsuite.com"},
+            {"label": "Blog", "url": "http://blog.hootsuite.com"}
           ]
         });
-    });
+      });
 
     $('._showImagePreview').click(function(){
         hsp.showImagePreview( 
